ec4795958890f4696f5c7b97c5b3ad3702887117
Aastha Mahindra <155626319+aasthamahindra@users.noreply.github.com>
2025-07-02 15:48:46 +0530

    Task form to add a new task

---
+++ frontend/src/components/TaskForm.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import axios from 'axios';
+import { useAuth } from "../context/AuthContext";
+
+export default function TaskForm({ onAdd }) {
+    const [form, setForm] = useState({ title: '', description: '' });
+    const { token } = useAuth();
+
+    const handleChange = (e) => {
+        setForm(prev => ({ ...prev, [e.target.name]: e.target.value }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!form.title.trim()) return;
+        try {
+            await axios.post('http://localhost:3000/api/tasks', form, {
+                headers: { Authorization: `Bearer ${token}`},
+            });
+            setForm({ title: '', description: '' });
+            onAdd();
+        } catch (err) {
+            alert('Error adding task');
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input name="title" placeholder="Task Title" value={form.title} onChange={handleChange} required />
+            <input name="description" placeholder="Description" value={form.description} onChange={handleChange} required />
+            <button type="submit">Add Task</button>
+        </form>
+    );
+}
